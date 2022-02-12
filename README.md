c970f211d33738357c6480523022ead204bfe672
Destinee A. Adams <adestinee37@gmail.com>
2022-02-12 13:20:08 -0500

    started the work on css

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 
 
 
-useful links
+### useful links
 - https://docs.python-requests.org/en/latest/
 - https://www.dataquest.io/blog/python-api-tutorial/
 - https://www.pythontutorial.net/python-basics/python-read-text-file/
 - https://rapidapi.com
 - https://pythonspot.com/read-file/
+- https://www.youtube.com/watch?v=wOr6PkZwEbk
+
+
+### note
+get more common words
+
+### color pallet
+    #F6F7EB
+    #E94F37
+    #393E41
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from flask import Flask, render_template
 import getAPIData as gad
 
-values = dict()
+app = Flask(__name__)
 
-values = gad.getData()
+if __name__ == '__app__':
+    app.run(debug=True)
 
-print(values)
+
+
+
+@app.route('/')
+def index():
+    content = gad.getData()
+    
+    '''somting i get an index error 
+        IndexError
+        IndexError: list index out of range
+        
+        need the learn how to stop it from rectin the program
+    '''
+    return render_template("index.html", term = content["term"], defintion = content["defintion"], gif = str(content["gif"]))
+
+
