f98a107be7fbe34233e0d6cf06001c22eeaa9c11
Destinee A. Adams <adestinee37@gmail.com>
2022-07-12 07:41:00 -0400

    fix the 404 errors
    words API didn't always give a word that has a definition. I fixed it by using two API instead of one Random Words and wordsAPI app is running smoothly now 😊

--- getAPIData.py
+++ getAPIData.py
@@ -1,136 +1,83 @@
 from pickle import NONE
 from urllib import response
-import requests
-import os
+from itsdangerous import json
+import requests, os, json
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env
 
 
-data = {"term": None, "defintion": None, "gif": None}
+data = {"term": "share", "backupterm": None,"defintion": None, "gif": None}
+
+rawData = None
 
 keys = {"WordsAPI": os.getenv("WordsAPI"), "gihpy": os.getenv("gihpy")}
 
 
+    
+# from randon word api
+def getJsonRandomWords():
+    global data
 
+    url = "https://random-words5.p.rapidapi.com/getRandom"
 
-# use this funtion when use as module 
+    headers = {
+        "X-RapidAPI-Key": os.getenv("RapidAPIKey"),
+        "X-RapidAPI-Host": "random-words5.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    response = response.text
+
+    
+    data["term"] = response
+    
+
+# from words api
+def getJsonDefinitions():
+    global data
+
+    url = "https://wordsapiv1.p.rapidapi.com/words/" + data["term"] + "/definitions"
+
+    headers = {
+        "X-RapidAPI-Key": os.getenv("RapidAPIKey"),
+        "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    response = response.json()
+    response = response["definitions"][0]["definition"]
+
+    data["defintion"] = response
+
+# from gihpy api
+def getJsonGifpy():
+    global data
+
+    url = "https://giphy.p.rapidapi.com/v1/gifs/search"
+
+    querystring = {"q":data["term"],"api_key": os.getenv("gihpy")}
+
+    headers = {
+        "X-RapidAPI-Key":   os.getenv("gihpyRapidAPIkey"),
+        "X-RapidAPI-Host": "giphy.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    
+    response = response.json()
+    response = response["data"][0]["embed_url"]
+    print(response)
+
+    data["gif"] = response
+
 def getData():
-    setAPI_KEYS()
-
-    getSetWord()
-    # getSetDef()
-    getSetGif()
+    getJsonRandomWords()
+    getJsonDefinitions()
+    getJsonGifpy()
 
     return data
 
-
-def setAPI_KEYS():
-    f = open("secret.txt","r")
-    lines = f.readlines()
-
-    keys["WordsAPI"] = lines[0].replace("\n","")
-    keys["gihpy"] = lines[1]
-
-    f.close()
-
-
-def setData(key, vulue):
-    data[key] = vulue
-    
-
-    
-def clearDatalist(key):
-    setData(str(key), None)
-
-# when something goes wrong fall back on this
-def setData404(method):
-    if(method == 'term'):
-        setData("term", "404 error")
-
-    if(method == 'defintion'):
-        setData("defintion", "trigger for an error 404 message is when website content has been removed or moved to another URL.")
-
-    if(method == 'gif'):
-        setData("gif", "https://giphy.com/embed/kF0ngyP7S1DfmzKqiN")
-
-    
-    
-
-def getSetWord():
-
-    clearDatalist("term")
-    
-    try: 
-        url = "https://wordsapiv1.p.rapidapi.com/words/"
-
-        querystring = {"random":"true"}
-
-        headers = {
-            'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
-            'x-rapidapi-key': keys["WordsAPI"]
-        }
-
-        r = requests.request("GET", url, headers=headers, params=querystring)
-
-
-
-        resjson = r.json()["word"]
-        setData("term", resjson)
-        
-        resjson = r.json()["results"][0]["definition"]
-        setData("defintion", resjson)
-
-    except IndexError: 
-        print('IndexError, getSetGif')
-        setData404("term")
-        setData404("defintion")
-        print(r.status_code, " from word")
-
-
-    except KeyError: 
-        print('IndexError, getSetGif')
-        setData404("term")
-        setData404("defintion")
-        print(r.status_code, " from word")
-
-def getSetDef():
-
-    return NONE
-
-def getSetGif():
-    params = {
-        "q": data["term"],
-        "apiKey":keys['gihpy'],
-        "limit":"2"
-    }
-
-    try:
-        url = 'https://api.giphy.com/v1/gifs/search?api_key=' + str(params["apiKey"]) + '&limit=' + str(params["limit"]) + '&q=' + str(params["q"])
-        
-        r = requests.get(url)
-
-        print(r.json()["data"][1]["embed_url"])
-
-        resjson = r.json()["data"][0]["embed_url"]
-        setData("gif", resjson)
-
-
-    except IndexError: 
-        print('IndexError, getSetGif')
-        setData404("gif")
-        print(r.status_code, " from gif")
-
-    except KeyError: 
-        print('IndexError, getSetGif')
-        setData404("gif")
-        print(r.status_code, " from gif")
-    
-
-
-    
-
-# getSetWord()
-# getSetDef()
-# getSetGif()
 
 print(getData())
