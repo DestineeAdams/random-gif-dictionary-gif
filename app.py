0fb753b61bb20b9f08396da8df3d92782ee1ebd5
Destinee A. Adams <adestinee37@gmail.com>
2022-07-12 09:50:33 -0400

    first deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import getAPIData as gad
 app = Flask(__name__)
 
 if __name__ == '__app__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 
@@ -14,6 +14,6 @@ if __name__ == '__app__':
 @app.route('/')
 def index():
     content = gad.getData()
-    
+
     return render_template("index.html", term = content["term"], defintion = content["defintion"], gif = str(content["gif"]))
 
