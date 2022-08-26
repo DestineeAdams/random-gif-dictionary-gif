d2906e82c793495a1a29bb9d61949c5970e5dd11
Destinee A. Adams <adestinee37@gmail.com>
2022-08-25 23:23:20 -0400

    changing profile

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder='./front/build', static_url_path='/')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
 
 @app.route('/words', methods=['GET'])
 def words():
