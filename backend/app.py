b39c46a1c08a6866e1e00013ec35d3c7d1829d26
Destinee A. Adams <adestinee37@gmail.com>
2022-08-26 00:00:15 -0400

    adding cors and cross_origin

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,23 @@
 from flask import Flask
 import getAPIData as gad
+from flask_cors import CORS, cross_origin
 
 
-app = Flask(__name__, static_folder='./front/build', static_url_path='/')
+app = Flask(__name__, static_folder='./front/build', static_url_path='')
+cors = CORS(app)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/words', methods=['GET'])
+@cross_origin()
 def words():
     content = gad.getData()
     return content
 
+@app.route("/")
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == "__main__":
+    app.run()
