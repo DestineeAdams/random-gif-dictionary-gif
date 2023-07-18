e062e0c788d0fffd8de47127631caedf72697108
Destinee A. Adams <adestinee37@gmail.com>
2023-07-17 22:45:12 -0400

    basic templete

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import datetime
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-    # return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+ 
+if __name__ == '__main__':
+   app.run(debug=true)
